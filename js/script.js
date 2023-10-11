b1270129b34a38af3c61b22b64a2650ae425764f
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-11 16:05:45 +0200

    modifing cicle js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
-const wrapperEl = document.getElementById('wrapper');
+const wrapperEl = document.getElementById('row');
 
-for(let i = 1; i <= 15; i ++){
-    for( let c = 1; c <= 7; c++){
-        let bgBox; 
-        if((c * i) % 15 == 0){
-            bgBox = 'fizzbuzz';
-        } else if((c * i) % 5 == 0){
-            bgBox = 'buzz';
-        } else if((c * i) % 3 == 0){
-            bgBox = 'fizz';
-        } else {
-            bgBox = 'box-normal';
-        }
+for(let i = 1; i <= 100; i ++){
 
-        console.log(c * i)
-        let boxEl = document.createElement('div');
-        boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center`;
-        console.log(bgBox)
-        boxEl.innerHTML = c * i;
-        wrapperEl.append(boxEl)
+    let bgBox; 
+    if(i % 15 == 0){
+        bgBox = 'fizzbuzz';
+    } else if(i % 5 == 0){
+        bgBox = 'buzz';
+    } else if(i % 3 == 0){
+        bgBox = 'fizz';
+    } else {
+        bgBox = 'box-normal';
     }
+
+    console.log(i)
+    let boxEl = document.createElement('div');
+    boxEl.className = `box ${bgBox} d-flex justify-content-center align-items-center`;
+    console.log(bgBox)
+    boxEl.innerHTML =i;
+    wrapperEl.append(boxEl)
 }
